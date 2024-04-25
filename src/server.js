01ac788eb238ddd0015bd6569dae6c3a8a4726a8
Nguyễn Văn Tín <126067526+nvtin1104@users.noreply.github.com>
2024-04-25 17:48:36 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,3 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
 
 import express from 'express'
 import { mapOrder } from '~/utils/sorts.js'
@@ -15,11 +10,11 @@ const port = 8017
 app.get('/', (req, res) => {
   // Test Absolute import mapOrder
   console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
+    [{ id: 'id-1', name: 'One' },
       { id: 'id-2', name: 'Two' },
       { id: 'id-3', name: 'Three' },
       { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
+      { id: 'id-5', name: 'Five' }],
     ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
     'id'
   ))
@@ -27,6 +22,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
+  console.log(`Server running at http://${hostname}:${port}/`)
+  console.log('Press Ctrl+C to quit.')
 })
